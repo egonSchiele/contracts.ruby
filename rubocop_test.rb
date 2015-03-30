697df7c56c7950eafdb3a73bc4b11da4e1930438
Aditya Bhargava <bluemangroupie@gmail.com>
2015-03-30 09:22:48 -0700

    fixes from rubocop

--- rubocop_test.rb
+++ rubocop_test.rb
@@ -1,6 +1,5 @@
 if RUBY_VERSION.to_f >= 2.1
   puts "running rubocop..."
   puts `bundle exec rubocop --config rubocop.yml #{ARGV.join(" ")}`
-  exit $?.exitstatus
+  exit $CHILD_STATUS.exitstatus
 end
-
