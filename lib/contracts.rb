b06a6fc2145f736c38844df700cd12b680bf56f2
Oleksii Fedorov <waterlink000@gmail.com>
2015-05-27 01:12:13 +0200

    fixes #154: Don't include contracts internals into users classes

--- lib/contracts.rb
+++ lib/contracts.rb
@@ -17,6 +17,10 @@ module Contracts
     common(base)
   end
 
+  def self.append_features(base)
+    base.include(BuiltinContracts)
+  end
+
   def self.common(base)
     return if base.respond_to?(:Contract)
 
