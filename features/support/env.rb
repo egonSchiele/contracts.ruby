ea063ab8a8c82eac88307e34d2525880ba93bcb1
Oleksii Fedorov <waterlink000@gmail.com>
2015-08-30 03:26:28 +0200

    Satisfy rubocop

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,5 @@ require "aruba/cucumber"
 require "aruba/jruby" if RUBY_PLATFORM == "java"
 
 Before do
-  @aruba_timeout_seconds = RUBY_PLATFORM == 'java' ? 30 : 5
+  @aruba_timeout_seconds = RUBY_PLATFORM == "java" ? 30 : 5
 end
